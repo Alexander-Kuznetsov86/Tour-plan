fe573f5f445953dce772fb5447b9360adeb81774
Alexander-Kuznetsov <aleksandrkv86@gmail.com>
2021-03-24 20:06:39 +0300

    create: added animation index.html

--- js/main.js
+++ js/main.js
@@ -34,25 +34,29 @@ $(document).ready(function () {
 
   var menuButton = $(".menu-button");
   menuButton.on("click", function () {
-    document
-      $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+    document;
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
 
-  var modalButton = $('[data-toggle=modal]');
-  var closeModalButton = $('.modal__close');
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
-    
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
   function openModal() {
-      var targetModal = $(this).attr('data-href');
-      $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
-      $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
-    }
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
   function closeModal(event) {
-      event.preventDefault();
-      var modalOverlay = $('.modal__overlay');
-      var modalDialog = $('.modal__dialog');
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
-    }
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  // Обработка форм
+  // $(".modal__form").validate();
+  // Маска для телефона
+  AOS.init();
 });
